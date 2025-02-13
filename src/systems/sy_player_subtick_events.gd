8db806a85d5aac47f4d9305433fe692605957064
woynert <woynertgamer@gmail.com>
2025-02-13 08:22:57 -0500

    fix: show lerped ball position client-side

--- src/systems/sy_player_subtick_events.gd
+++ src/systems/sy_player_subtick_events.gd
@@ -52,6 +52,8 @@ func on_process_entity(entity: Entity, _data, _delta: float):
 
 
 static func debug_show_timewarpable_lerped_positions(node_ctx: Node, wync_ctx: WyncCtx, entity_id: int):
+
+	# TODO: generalize with 'wync_lerp'
 	
 	var co_ticks = ECS.get_singleton_component(node_ctx, CoTicks.label) as CoTicks
 	var co_predict_data = ECS.get_singleton_component(node_ctx, CoSingleNetPredictionData.label) as CoSingleNetPredictionData
@@ -59,34 +61,31 @@ static func debug_show_timewarpable_lerped_positions(node_ctx: Node, wync_ctx: W
 	var curr_tick_time = ClockUtils.get_tick_local_time_msec(co_predict_data, co_ticks, co_ticks.ticks)
 	var curr_time = curr_tick_time + co_ticks.lerp_delta_accumulator_ms
 	
-	var snap_left: NetTickData = null
-	var snap_right: NetTickData = null
-	var found_snapshots = false
-	var using_confirmed_state = false
-	
-	
+	var snap_left_tick: int = 0
+	var snap_right_tick: int = 0
+	var snap_left: Variant = null
+	var snap_right: Variant = null
 	var prop = WyncUtils.entity_get_prop(wync_ctx, entity_id, "position")
-	
-	"""
-	var current_state = prop.getter.call()
-	if current_state is not Vector2:
-		return"""
-	
 	var target_time = curr_time - co_predict_data.lerp_ms
-	var snaps = WyncUtils.find_closest_two_snapshots_from_prop(target_time, prop, co_ticks, co_predict_data)
+	var snaps = WyncUtils.find_closest_two_snapshots_from_prop(wync_ctx, target_time, prop, co_ticks, co_predict_data)
 
 	if snaps.size() == 2:
-		snap_left = snaps[0] as NetTickData
-		snap_right = snaps[1] as NetTickData
-		using_confirmed_state = true
-		found_snapshots = true
+		snap_left_tick = snaps[0]
+		snap_right_tick = snaps[1]
+		snap_left = prop.confirmed_states.get_at(snap_left_tick)
+		snap_right = prop.confirmed_states.get_at(snap_right_tick)
+	else:
+		return
 	
-	var left_timestamp = ClockUtils.get_tick_local_time_msec(co_predict_data, co_ticks, snap_left.arrived_at_tick)
-	var right_timestamp = ClockUtils.get_tick_local_time_msec(co_predict_data, co_ticks, snap_right.arrived_at_tick)
+	var left_arrived_at_tick = prop.arrived_at_tick.get_at(snap_left_tick)
+	var right_arrived_at_tick = prop.arrived_at_tick.get_at(snap_right_tick)
+
+	var left_timestamp = ClockUtils.get_tick_local_time_msec(co_predict_data, co_ticks, left_arrived_at_tick)
+	var right_timestamp = ClockUtils.get_tick_local_time_msec(co_predict_data, co_ticks, right_arrived_at_tick)
 	var interpolated_state = Vector2.ZERO
 
 	if abs(left_timestamp - right_timestamp) < 0.000001:
-		interpolated_state = snap_right.data
+		interpolated_state = snap_right
 	else:
 		var factor = clampf(
 			(float(target_time) - left_timestamp) / (right_timestamp - left_timestamp),
@@ -94,24 +93,17 @@ static func debug_show_timewarpable_lerped_positions(node_ctx: Node, wync_ctx: W
 		
 		match prop.data_type:
 			WyncEntityProp.DATA_TYPE.FLOAT:
-				var left_pos = snap_left.data as float
-				var right_pos = snap_right.data as float
+				var left_pos = snap_left as float
+				var right_pos = snap_right as float
 				interpolated_state = lerp(left_pos, right_pos, factor)
 			WyncEntityProp.DATA_TYPE.VECTOR2:
-				var left_pos = snap_left.data as Vector2
-				var right_pos = snap_right.data as Vector2
+				var left_pos = snap_left as Vector2
+				var right_pos = snap_right as Vector2
 				interpolated_state = left_pos.lerp(right_pos, factor)
 			_:
 				Log.out(node_ctx, "W: data type not interpolable")
 				pass
 	
-	#Wync
-	#var last_tick_rendered_left = ClockUtils.convert_local_ticks_to_server_ticks(co_ticks, snap_left.server_tick)
-	var last_tick_rendered_left = snap_left.server_tick
-	Log.out(node_ctx, "EVENT | snap_left_tick local:%s | converted:%s" % [ snap_left.server_tick, last_tick_rendered_left ])
+	var last_tick_rendered_left = snap_left_tick
+	Log.out(node_ctx, "EVENT | snap_left_tick local:%s | converted:%s" % [ snap_left_tick, last_tick_rendered_left ])
 	DebugPlayerTrail.spawn(node_ctx, interpolated_state, 0.9, 2.5)
-	
-	# print in console the exact position for this tick
-	
-	Log.out(node_ctx, "Client pos at tick %s is %s" % [last_tick_rendered_left, prop.confirmed_states.get_at(last_tick_rendered_left).data])
-		
