09785e782a24855f258593ad90e1a7b08df5f237
woynert <woynertgamer@gmail.com>
2025-02-24 21:03:06 -0500

    feat(extrapolation): split xtrap logic into user callable functions for customized flow

--- src/network/wync/sy_wync_xtrap.gd
+++ src/network/wync/sy_wync_xtrap.gd
@@ -16,18 +16,77 @@ func _ready():
 func on_process(entities, _data, delta: float):
 
 	var single_wync = ECS.get_singleton_component(self, CoSingleWyncContext.label) as CoSingleWyncContext
-	var wync_ctx = single_wync.ctx as WyncCtx
-	var co_ticks = wync_ctx.co_ticks
-	var co_predict_data = wync_ctx.co_predict_data
+	var ctx = single_wync.ctx as WyncCtx
+	var last_confirmed_tick = ctx.last_tick_received
+	var target_tick = ctx.co_predict_data.target_tick
 
-	var co_loopback = GlobalSingletons.singleton.get_component(CoTransportLoopback.label) as CoTransportLoopback
-	if not co_loopback:
-		Log.err("Couldn't find singleton CoTransportLoopback", Log.TAG_XTRAP)
+	# get physics space to later sync transforms to physics server
+	# sync physics after 'SyWyncLatestValue'
+	
+	var space := get_viewport().world_2d.space
+	RapierPhysicsServer2D.space_step(space, 0)
+	RapierPhysicsServer2D.space_flush_queries(space)
+
+	if WyncXtrap.wync_xtrap_preparation(ctx) != OK:
 		return
+
+	for tick in range(last_confirmed_tick +1, target_tick +1):
+
+		if WyncXtrap.wync_xtrap_tick_init(ctx, tick) != OK:
+			continue
+
+		# ------- START USER PREDICTION FUNCTIONS -------
+		# All state modified here is Predicted / Extrapolated
+		
+		for entity: Entity in entities:
+		
+			var co_actor = entity.get_component(CoActor.label) as CoActor
+			if !WyncUtils.entity_is_predicted(ctx, co_actor.id):
+				continue
+			if ECS.entity_has_system_components(entity.id, SyActorMovement.label):
+				SyActorMovement.simulate_movement(entity, delta)
+				SyActorMovement.simulate_particle_on_start_moving(entity, delta, tick)
+			if ECS.entity_has_system_components(entity.id, SyBallMovement.label):
+				SyBallMovement.simulate_movement(entity, delta)
+		
+		SyActorEvents.client_simulate_events(self)
+		
+		# debug: show a player trail
+
+		for entity: Entity in entities:
+			var co_actor = entity.get_component(CoActor.label) as CoActor
+			if !WyncUtils.entity_is_predicted(ctx, co_actor.id):
+				continue
+			if tick == target_tick || tick == last_confirmed_tick +1:
+				var progress = (float(tick) - last_confirmed_tick) / (target_tick - last_confirmed_tick)
+				var prop_position = WyncUtils.entity_get_prop(ctx, co_actor.id, "position")
+				if prop_position:
+					DebugPlayerTrail.spawn(self, prop_position.getter.call(), progress)
+
+		# ------- END USER PREDICTION FUNCTIONS -------
+
+		WyncXtrap.wync_xtrap_tick_end(ctx, tick)
+
+		# sync transforms to physics server
+		RapierPhysicsServer2D.space_step(space, 0)
+		RapierPhysicsServer2D.space_flush_queries(space)
+
+	WyncXtrap.wync_xtrap_termination(ctx)
+
+
+
+
+"""
+func on_process_inlined(entities, _data, delta: float):
+
+	var single_wync = ECS.get_singleton_component(self, CoSingleWyncContext.label) as CoSingleWyncContext
+	var ctx = single_wync.ctx as WyncCtx
+	var co_ticks = ctx.co_ticks
+	var co_predict_data = ctx.co_predict_data
 
 	var target_tick = co_predict_data.target_tick
 
-	Log.out("debug1 | xtrap (init) lo_tick(%s) ser_ticks(%s) offset(%s) target_tick(%s)" % [co_ticks.ticks, co_ticks.server_ticks, co_predict_data.tick_offset, target_tick], Log.TAG_XTRAP)
+	#Log.out("debug1 | xtrap (init) lo_tick(%s) ser_ticks(%s) offset(%s) target_tick(%s)" % [co_ticks.ticks, co_ticks.server_ticks, co_predict_data.tick_offset, target_tick], Log.TAG_XTRAP)
 	
 	# get physics space to later sync transforms to physics server
 	# sync physics after 'SyWyncLatestValue'
@@ -40,12 +99,12 @@ func on_process(entities, _data, delta: float):
 	# FIXME: using last_confirmed_tick is UB
 	# FIXME: not all props will have the same 'last_confirmed_tick'
 
-	var last_confirmed_tick = wync_ctx.last_tick_received
+	var last_confirmed_tick = ctx.last_tick_received
 	if last_confirmed_tick == 0:
 		return
 
-	wync_ctx.currently_on_predicted_tick = true
-	# NOTE: defer: wync_ctx.currently_on_predicted_tick = false
+	ctx.currently_on_predicted_tick = true
+	# NOTE: defer: ctx.currently_on_predicted_tick = false
 	
 	# We can detect a local_tick is duplicated by checking is the same as te previous,
 	# Then we can honor the config wheter to allow duplication or not for each prop
@@ -54,7 +113,7 @@ func on_process(entities, _data, delta: float):
 	var local_tick: Variant = null # Optional<int>
 	
 	for tick in range(last_confirmed_tick +1, target_tick +1):
-		wync_ctx.current_predicted_tick = tick
+		ctx.current_predicted_tick = tick
 		
 		# set events inputs to corresponding value depending on tick
 		# --------------------------------------------------
@@ -67,12 +126,12 @@ func on_process(entities, _data, delta: float):
 			continue
 		is_local_tick_duplicated = prev_local_tick == local_tick
 		
-		for prop_id: int in range(wync_ctx.props.size()):
+		for prop_id: int in range(ctx.props.size()):
 			
-			var prop = wync_ctx.props[prop_id] as WyncEntityProp
+			var prop = ctx.props[prop_id] as WyncEntityProp
 			if prop == null:
 				continue
-			if not WyncUtils.prop_is_predicted(wync_ctx, prop_id):
+			if not WyncUtils.prop_is_predicted(ctx, prop_id):
 				continue
 			if prop.data_type not in [WyncEntityProp.DATA_TYPE.INPUT,
 				WyncEntityProp.DATA_TYPE.EVENT]:
@@ -98,7 +157,7 @@ func on_process(entities, _data, delta: float):
 
 		# clearing delta events before predicting, predicted delta events will be
 		# polled and cached at the end of the predicted tick
-		SyWyncTickStartAfter.auxiliar_props_clear_current_delta_events(wync_ctx)
+		SyWyncTickStartAfter.auxiliar_props_clear_current_delta_events(ctx)
 		
 		# START USER PREDICTION FUNCTIONS --------------------------------------
 		# Prediction / Extrapolation:
@@ -108,7 +167,7 @@ func on_process(entities, _data, delta: float):
 		for entity: Entity in entities:
 		
 			var co_actor = entity.get_component(CoActor.label) as CoActor
-			if !WyncUtils.entity_is_predicted(wync_ctx, co_actor.id):
+			if !WyncUtils.entity_is_predicted(ctx, co_actor.id):
 				continue
 			
 			if ECS.entity_has_system_components(entity.id, SyActorMovement.label):
@@ -129,46 +188,46 @@ func on_process(entities, _data, delta: float):
 		for entity: Entity in entities:
 		
 			var co_actor = entity.get_component(CoActor.label) as CoActor
-			if !WyncUtils.entity_is_predicted(wync_ctx, co_actor.id):
+			if !WyncUtils.entity_is_predicted(ctx, co_actor.id):
 				continue
 			
 			# store predicted states
 			# (run on last two iterations)
 			
 			if tick > (target_tick -1):
-				props_update_predicted_states_data(wync_ctx, wync_ctx.entity_has_props[co_actor.id])
+				props_update_predicted_states_data(ctx, ctx.entity_has_props[co_actor.id])
 				
 			# debug player trail
 			
 			if tick == target_tick || tick == last_confirmed_tick +1:
 				var progress = (float(tick) - last_confirmed_tick) / (target_tick - last_confirmed_tick)
-				var prop_position = WyncUtils.entity_get_prop(wync_ctx, co_actor.id, "position")
+				var prop_position = WyncUtils.entity_get_prop(ctx, co_actor.id, "position")
 				if prop_position:
 					DebugPlayerTrail.spawn(self, prop_position.getter.call(), progress)
 
 			# integration functions
 			
-			var int_fun = WyncUtils.entity_get_integrate_fun(wync_ctx, co_actor.id)
+			var int_fun = WyncUtils.entity_get_integrate_fun(ctx, co_actor.id)
 			if int_fun is Callable:
 				int_fun.call()
 
 			# update/store predicted state metadata
 		
-			props_update_predicted_states_ticks(wync_ctx, wync_ctx.entity_has_props[co_actor.id], target_tick)
+			props_update_predicted_states_ticks(ctx, ctx.entity_has_props[co_actor.id], target_tick)
 		
 		# extract / poll for generated predicted _undo delta events_
 		
-		for prop_id: int in range(wync_ctx.props.size()):
-			var prop = WyncUtils.get_prop(wync_ctx, prop_id)
+		for prop_id: int in range(ctx.props.size()):
+			var prop = WyncUtils.get_prop(ctx, prop_id)
 			if prop == null:
 				continue
 			prop = prop as WyncEntityProp
 			if not prop.relative_syncable:
 				continue
-			if not WyncUtils.prop_is_predicted(wync_ctx, prop_id):
+			if not WyncUtils.prop_is_predicted(ctx, prop_id):
 				continue
 				
-			var aux_prop = WyncUtils.get_prop(wync_ctx, prop.auxiliar_delta_events_prop_id)
+			var aux_prop = WyncUtils.get_prop(ctx, prop.auxiliar_delta_events_prop_id)
 			if aux_prop == null:
 				continue
 			aux_prop = aux_prop as WyncEntityProp
@@ -181,47 +240,9 @@ func on_process(entities, _data, delta: float):
 		RapierPhysicsServer2D.space_step(space, 0)
 		RapierPhysicsServer2D.space_flush_queries(space)
 
-	SyWyncTickStartAfter.auxiliar_props_clear_current_delta_events(wync_ctx)
+	SyWyncTickStartAfter.auxiliar_props_clear_current_delta_events(ctx)
 	co_predict_data.delta_prop_last_tick_predicted = target_tick
 
 	# "defer"
-	wync_ctx.currently_on_predicted_tick = false
-
-
-static func props_update_predicted_states_data(ctx: WyncCtx, props_ids: Array) -> void:
-	
-	for prop_id: int in props_ids:
-		
-		var prop = ctx.props[prop_id] as WyncEntityProp
-		if prop == null:
-			continue
-
-		var pred_curr = prop.pred_curr
-		var pred_prev = prop.pred_prev
-		
-		# Initialize stored predicted states. TODO: Move elsewhere
-		
-		if pred_curr.data == null:
-			pred_curr.data = Vector2.ZERO
-			pred_prev = pred_curr.copy()
-			continue
-			
-		# store predicted states
-		# (run on last two iterations)
-		
-		pred_prev.data = pred_curr.data
-		pred_curr.data = prop.getter.call()
-
-
-static func props_update_predicted_states_ticks(ctx: WyncCtx, props_ids: Array, target_tick: int) -> void:
-	
-	for prop_id: int in props_ids:
-		
-		var prop = ctx.props[prop_id] as WyncEntityProp
-		if prop == null:
-			continue
-
-		# update store predicted state metadata
-		
-		prop.pred_prev.server_tick = target_tick -1
-		prop.pred_curr.server_tick = target_tick
+	ctx.currently_on_predicted_tick = false
+"""
