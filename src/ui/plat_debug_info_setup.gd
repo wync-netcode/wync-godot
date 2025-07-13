8b6acf8e14916422bda7f06d51211d7d9fd70dc9
woynert <woynertgamer@gmail.com>
2025-07-13 15:43:03 -0500

    feat(debug): toggle info

--- src/ui/plat_debug_info_setup.gd
+++ src/ui/plat_debug_info_setup.gd
@@ -3,6 +3,7 @@ extends Node
 
 @onready var server = %"Server"
 @onready var client = %"Client"
+var show_debug_info: bool = true
 
 
 func _ready():
@@ -16,3 +17,17 @@ func recursively_initialize_debug_info(node: Node):
 			child.initialize(PlatGlobals.loopback_ctx, server.gs.wctx, client.gs.wctx)
 		else:
 			recursively_initialize_debug_info(child)
+
+
+func recursively_change_visibility(node: Node):
+	for child in node.get_children():
+		if child is DynamicDebugInfo:
+			child.visible = show_debug_info
+		else:
+			recursively_change_visibility(child)
+
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("key_debug_3"):
+		show_debug_info = not show_debug_info
+		recursively_change_visibility(self)
